605c281141c7fee6cb84434b3e11f61b5d112064
Alfio Lazzaro <alfio.lazzaro@chem.uzh.ch>
2018-05-25 10:25:57 +0200

    update check archives to use a single library

--- tools/build_utils/check_archives.py
+++ tools/build_utils/check_archives.py
@@ -7,6 +7,7 @@ import sys, os
 import subprocess
 from os import path
 from glob import glob
+from sets import Set
 
 KNOWN_EXTENSIONS = ("F", "c", "cu", "cpp", "cxx", "cc", )
 
@@ -20,6 +21,8 @@ def main():
     src_dir = sys.argv[2]
     lib_dir = sys.argv[3]
 
+    # Search all files belonging to a given archive
+    archives_files={}
     for root, dirs, files in os.walk(src_dir):
         if "PACKAGE" in files:
             content = open(path.join(root,"PACKAGE")).read()
@@ -29,21 +32,29 @@ def main():
             if "archive" in package.keys():
                 archive = package["archive"]
 
-            archive_fn = path.join(lib_dir, archive+".a")
-            if(not path.exists(archive_fn)):
-                continue
-
             file_parts = [fn.rsplit(".", 1) for fn in files]
             src_basenames = [parts[0] for parts in file_parts if parts[-1] in KNOWN_EXTENSIONS]
 
-            output = check_output([ar_exe, "t", archive_fn])
-            for line in output.strip().split("\n"):
-                if(line == "__.SYMDEF SORTED"): continue  # needed for MacOS
-                assert(line.endswith(".o"))
-                if(line[:-2] not in src_basenames):
-                    print("Could not find source for object %s in archive %s , removing archive."%(line, archive_fn))
-                    os.remove(archive_fn)
-                    break
+            if archive in archives_files:
+                archives_files[archive] |= Set(src_basenames)
+            else:
+                archives_files[archive] = Set(src_basenames)
+
+    # Check if the symbols in each archive have a corresponding source file
+    for archive in archives_files:
+        archive_fn = path.join(lib_dir, archive+".a")
+        if(not path.exists(archive_fn)):
+            continue
+
+        output = check_output([ar_exe, "t", archive_fn])
+        for line in output.strip().split("\n"):
+            if(line == "__.SYMDEF SORTED"): continue  # needed for MacOS
+            assert(line.endswith(".o"))
+            if(line[:-2] not in archives_files[archive]):
+                print("Could not find source for object %s in archive %s , removing archive."%(line, archive_fn))
+                os.remove(archive_fn)
+                break
+
 
 #=============================================================================
 def check_output(*popenargs, **kwargs):
