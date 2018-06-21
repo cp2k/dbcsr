5761f8f33fdf05c02bb14be949fb113daa252a80
shoshijak <as.jak@protonmail.ch>
2018-06-21 12:02:11 +0200

    JIT-ted kernel validation check

--- src/acc/libsmm_acc/libcusmm/libcusmm_benchmark.cu
+++ src/acc/libsmm_acc/libcusmm/libcusmm_benchmark.cu
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <cuda.h>
 #include "libcusmm_benchmark.h"
+#include "libcusmm.h"
 
 //===========================================================================
 // Allocate memory and cuda events
@@ -82,108 +83,6 @@ void libcusmm_benchmark_finalize(libcusmm_benchmark_t* handle){
 
 
 //===========================================================================
-// initialize matrix
-static void matInit(double* mat, int mat_n, int x, int y, int seed){
-
- double *m = mat;
-
- for(int n=0; n<mat_n; n++){
-   for(int j=0; j<y; j++) {
-     for(int i=0; i<x; i++, m++) {
-     *m = (double) j*x + i + n + seed;
-     //printf("matrix [%d, %d]=%g\n", i, j, *m);
-     }
-   }
- }
-
-}
-
-
-//===========================================================================
-// initialize the task list ("stack" in CP2K lingo)
-// for each of the result matrices we have a random number
-static void stackInit(int *stack, int n_stack, int n_c, double* mat_c,
-               int n_a, double * mat_a, int n_b, double* mat_b,
-               int mat_m, int mat_n, int mat_k){
-
-  if(n_stack < n_c){
-    printf("Error: n_stack < n_c\n");
-    exit(1);
-  }
-
-  // on average, we have n_avg matrix products contributing to a result mat_c
-  int n_avg = n_stack / n_c;
-
-  int n_imbalance = max(1, n_avg-4);
-
-  int c = 0;
-  int n_top = 0;
-  int p = 0;
-
- int* s = stack;
-  while( p < n_stack ){
-    if(c >= n_c) c = n_c-1;
-
-    n_top += n_avg + (rand() % (2*n_imbalance) - n_imbalance); 
-    if(n_top > n_stack) n_top = n_stack;
-
-    for(;p < n_top; p++){
-     int a = rand() % n_a;
-     int b = rand() % n_b;
-
-     *s++ =  a * mat_m * mat_k + 1;        // A_src
-     *s++ =  b * mat_k * mat_n + 1;        // B_src
-     *s++ =  c * mat_m * mat_n + 1;        // C_dst
-    }
-    c++;
- }
-}
-
-
-//===========================================================================
-static void stackCalc(int* stack, int n_stack, double* mat_c, double *mat_a, double* mat_b,
-               int mat_m, int mat_n, int mat_k){
-
-  for(int s=0; s<n_stack; s++){
-     int a_base = stack[3 * s    ] - 1;
-     int b_base = stack[3 * s + 1] - 1;
-     int c_base = stack[3 * s + 2] - 1;
-
-     for(int n=0; n<mat_n; n++){
-       for(int m=0; m<mat_m; m++){
-         double res = 0.;
-         for(int k=0; k<mat_k; k++){
-          int a_ind = k * mat_m + m;
-
-
-//         // initialize with non-transpose matrix
-//         int b_ind = n * mat_k + k;
-//         res += mat_a[a_base + a_ind] * mat_b[b_base + b_ind];
-
-          // initialize with transpose matrix
-          int b_ind = k * mat_n + n;
-          res += mat_a[a_base + a_ind] * mat_b[b_base + b_ind];
-         }
-         int c_ind = n * mat_m +  m;
-         mat_c[c_base + c_ind] += res;
-       }
-     }
-  }
-
-}
-
-
-//===========================================================================
-static double checkSum(double* mat_c, int n_c, int mat_m, int mat_n){
-   double res = 0;
-   for(int i=0; i<n_c * mat_m * mat_n; i++){
-     res += mat_c[i];
-   }
-   return res;
-}
-
-
-//===========================================================================
 //Removes special symbols so that the output is usefull for awk and gnuplot.
 static void clean_string(char* str_in, char* str_out){
     for(int i=0; i<1000 ; i++){
@@ -216,7 +115,7 @@ int libcusmm_benchmark(libcusmm_benchmark_t* h,
      n_warm = min(3, n_iter);
  }
 
- const int stream = 0;
+ CUstream stream;
 
  int error_counter = 0;
  int best_kernel = -1;
@@ -251,7 +150,7 @@ int libcusmm_benchmark(libcusmm_benchmark_t* h,
     //warmup run (more often if n_iter is small)
     for(int i=0; i<n_warm; i++)
         launchers[ikern](h->d_stack, h->n_stack, stream, mat_m, mat_n, mat_k, h->d_mat_a, h->d_mat_b, h->d_mat_c);
-    cudaMemset(h->d_mat_c, stream, h->n_c * mat_m * mat_n * sizeof(double));
+    cudaMemset(h->d_mat_c, 0, h->n_c * mat_m * mat_n * sizeof(double));
 
     cudaEventRecord(h->t_start, stream);
 
