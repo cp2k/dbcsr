959fe9a6b475e6326eb499206000692c55b75b8f
Alfio Lazzaro <alfio.lazzaro@gmail.com>
2018-08-22 22:26:55 +0200

    Consider correct transpose case for CUBLAS DGEMM

--- src/acc/cublaswrap/cublas.cu
+++ src/acc/cublaswrap/cublas.cu
@@ -43,19 +43,22 @@ extern "C" int cublas_dgemm_loop(cublasHandle_t *handle, char transa, char trans
   }
   cublasOperation_t cTransa = transa=='N' ? CUBLAS_OP_N : CUBLAS_OP_T;
   cublasOperation_t cTransb = transb=='N' ? CUBLAS_OP_N : CUBLAS_OP_T;
+  int m, n, k;
+  int &lda = transa=='N' ? m : k;
+  int &ldb = transb=='N' ? k : n;
 
   for (int ii = 0; ii < stack_size; ii++) {
     // get mnk from stack data
-    int m = stack_params[ ps_width * ii ];
-    int n = stack_params[ ps_width * ii + 1];
-    int k = stack_params[ ps_width * ii + 2];
- 
+    m = stack_params[ ps_width * ii ];
+    n = stack_params[ ps_width * ii + 1];
+    k = stack_params[ ps_width * ii + 2];
+
     // get first element of data, index - 1 becasue data comes from fortran
     double *a_mat = &a_data[ stack_params[ ps_width * ii + 3 ] - 1 ];
     double *b_mat = &b_data[ stack_params[ ps_width * ii + 4 ] - 1 ];
     double *c_mat = &c_data[ stack_params[ ps_width * ii + 5 ] - 1 ];
-    
-    cublasStatus_t stat = cublasDgemm(*handle, cTransa, cTransb, m, n, k, &alpha, a_mat, m, b_mat, k, &beta, c_mat, m);
+
+    cublasStatus_t stat = cublasDgemm(*handle, cTransa, cTransb, m, n, k, &alpha, a_mat, lda, b_mat, ldb, &beta, c_mat, lda);
     if (stat != CUBLAS_STATUS_SUCCESS) return(-1);
   }
   if (cuda_error_check(cudaGetLastError())) return(-1);
