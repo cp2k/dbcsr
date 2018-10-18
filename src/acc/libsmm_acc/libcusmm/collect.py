48d7fdfc846fb2b804ad07b6c874ab52cbd415f8
shoshijak <as.jak@protonmail.ch>
2018-10-19 00:07:24 +0200

    libcusmm: remove redundant parentheses

--- src/acc/libsmm_acc/libcusmm/collect.py
+++ src/acc/libsmm_acc/libcusmm/collect.py
@@ -31,13 +31,13 @@ def main():
     winners = dict()
 
     for d in glob("tune_*"):
-        if(not os.path.isdir(d)):
+        if not os.path.isdir(d):
             continue
 
         for exe_fn in glob(d+"/tune_*main.cu"):
             mnk = tuple([int(i) for i in re_mnk.search(exe_fn).groups()])
             log_fn = exe_fn.replace("_main.cu", ".log")
-            if(not os.path.exists(log_fn)):
+            if not os.path.exists(log_fn):
                 winners[mnk] = "log missing: "+log_fn
                 continue
 
@@ -63,31 +63,31 @@ def process_log(log_fn, mnk, winners):
     f.close()
 
     m = re_errors.search(content)
-    if(not m):
+    if not m:
         winners[mnk] = "log incomplete: "+log_fn
         return
 
     n_errors = int(m.group(1))
-    if(n_errors != 0):
+    if n_errors != 0:
         winners[mnk] = "errors: "+log_fn
         return
 
     old_gflops = 0.0
-    if(mnk in winners.keys()):
+    if mnk in winners.keys():
         m = re_gflops.search(winners[mnk])
-        if(not m):
+        if not m:
             return
         old_gflops = float(m.group(1))
 
     new_winner = re_winner.search(content).group(1).strip().replace("GFlops","GFlop/s")
     new_gflops = float(re_gflops.search(new_winner).group(1))
 
-    if(new_gflops > old_gflops):
+    if new_gflops > old_gflops :
         winners[mnk] = new_winner
 
 
 #===============================================================================
-if(len(sys.argv)==2 and sys.argv[-1]=="--selftest"):
+if len(sys.argv)==2 and sys.argv[-1]=="--selftest":
     pass #TODO implement selftest
 else:
     main()
--- src/acc/libsmm_acc/libcusmm/merge.py
+++ src/acc/libsmm_acc/libcusmm/merge.py
@@ -79,7 +79,7 @@ def main():
     print("Wrote parameters.new")
 
 #===============================================================================
-if(len(sys.argv)==2 and sys.argv[-1]=="--selftest"):
+if len(sys.argv)==2 and sys.argv[-1]=="--selftest":
     pass #TODO implement selftest
 else:
     main()
