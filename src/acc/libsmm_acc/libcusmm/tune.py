87b9d6aa206bced98a54bbdc25f383e1f582b11f
shoshijak <as.jak@protonmail.ch>
2018-10-18 12:57:06 +0200

    libcusmm/tune: read autotuned parameters from JSON
    - avoid using eval() of a file passed as a command line argument
    (bad practice)

--- src/acc/libsmm_acc/libcusmm/tune.py
+++ src/acc/libsmm_acc/libcusmm/tune.py
@@ -3,6 +3,7 @@
 
 import sys
 import os
+import json
 from glob import glob
 from itertools import product
 from optparse import OptionParser
@@ -12,6 +13,20 @@ from kernels.cusmm_dnt_largeDB2 import Kernel_dnt_largeDB2
 from kernels.cusmm_dnt_medium   import Kernel_dnt_medium
 from kernels.cusmm_dnt_small    import Kernel_dnt_small
 from kernels.cusmm_dnt_tiny     import Kernel_dnt_tiny
+from kernels.cusmm_dnt          import *
+
+kernel_algorithm = {
+    'tiny': Kernel_dnt_tiny,
+    'small': Kernel_dnt_small,
+    'medium': Kernel_dnt_medium,
+    'largeDB1': Kernel_dnt_largeDB1,
+    'largeDB2': Kernel_dnt_largeDB2
+}
+
+
+def get_kernel(**params):
+    return kernel_algorithm[params['algorithm']](**params)
+
 
 ALL_KERNELS = (Kernel_dnt_tiny, Kernel_dnt_small, Kernel_dnt_medium, Kernel_dnt_largeDB1, Kernel_dnt_largeDB2,)
 
@@ -41,7 +56,7 @@ def main():
     param_fn = options.params
     assert param_fn in arch_number.keys(), "Cannot find compute version for file " + param_fn
     arch = arch_number[param_fn]
-    all_kernels = eval(open(param_fn).read())
+    all_kernels = [get_kernel(**params) for params in json.load(open(param_fn))]
     print("Libcusmm: Found %d existing parameter sets."%len(all_kernels))
 
     blocksizes = [int(i) for i in args[1:]]
