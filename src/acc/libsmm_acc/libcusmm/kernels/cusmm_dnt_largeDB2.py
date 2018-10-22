36fbe9cfa0e435846bee94cf6678dc16af650b66
shoshijak <as.jak@protonmail.ch>
2018-10-22 13:09:00 +0200

    libcusmm/autotune: merge cusmm_dnt and cusmm_common

--- src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt_largeDB2.py
+++ src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt_largeDB2.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from kernels import cusmm_dnt
-import cusmm_common as cu
+from kernels import cusmm_dnt as cu
 
 
-class Kernel_dnt_largeDB2(cusmm_dnt.Kernel):
+class Kernel_dnt_largeDB2(cu.Kernel):
 
     algorithm = "largeDB2"
     launch_parameters = ['m', 'n', 'k', 'tile_m', 'tile_n', 'w', 'v', 'threads', 'grouping', 'minblocks']
--- src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt_small.py
+++ src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt_small.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from kernels import cusmm_dnt
-import cusmm_common as cu
+from kernels import cusmm_dnt as cu
 
 
-class Kernel_dnt_small(cusmm_dnt.Kernel):
+class Kernel_dnt_small(cu.Kernel):
 
     algorithm = "small"
     launch_parameters = ['m', 'n', 'k', 'tile_m', 'tile_n', 'threads', 'grouping', 'minblocks']
