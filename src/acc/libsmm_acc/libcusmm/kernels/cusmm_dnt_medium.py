a4cc705f534e64fcd1432795d6fac7b8bf3ec839
shoshijak <as.jak@protonmail.ch>
2018-10-22 13:03:12 +0200

    libcusmm/kernels: format comments, spacing, parentheses

--- src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt_medium.py
+++ src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt_medium.py
@@ -18,9 +18,9 @@ class Kernel_dnt_medium(cusmm_dnt.Kernel):
         self.grouping = grouping
         self.minblocks = minblocks
         self.perf = perf
-        assert(self.threads * self.minblocks <= 2048)
+        assert self.threads * self.minblocks <= 2048
         min_threads = ((self.m + self.tile_m - 1) // self.tile_m) * ((self.n + self.tile_n - 1) // self.tile_n)
-        assert(min_threads <= self.threads)
+        assert min_threads <= self.threads
 
     @property
     def func_signature(self):
@@ -30,9 +30,9 @@ class Kernel_dnt_medium(cusmm_dnt.Kernel):
     @staticmethod
     def promising_parameters(m, n, k, gpu):
         params = []
-        for minblocks in range(1, 28): # for exhaustive search: range(1, gpu.maxBLOCKSperSM + 1):
-                                       # heuristic: the optimal minblocks is never > 28
-            if m >= 28: # heuristic: investigate a smaller search space of grouping for large matrices 
+        for minblocks in range(1, 28):  # for exhaustive search: range(1, gpu.maxBLOCKSperSM + 1):
+                                        # heuristic: the optimal minblocks is never > 28
+            if m >= 28:  # heuristic: investigate a smaller search space of grouping for large matrices
                 grouping_range = (3, 4, 5, 24, 26, 29, 32,)
             else: 
                 grouping_range = range(1, 32 + 1, 1)
@@ -40,8 +40,8 @@ class Kernel_dnt_medium(cusmm_dnt.Kernel):
                 for tm in range(1, min(12, m) + 1):  # heuristic: the optimal tile_m is never above 12
                     for tn in range(1, min(12, n) + 1):  # heuristic: the optimal tile_m is never above 12
 
-                        if (tm * tn > 16):
-                            continue # heuristic: performance decreases for very large tiles
+                        if tm * tn > 16:
+                            continue  # heuristic: performance decreases for very large tiles
 
                         # Number of tiled columns, rows
                         cmax = (n + tn - 1) // tn
@@ -50,16 +50,16 @@ class Kernel_dnt_medium(cusmm_dnt.Kernel):
                         # Max work ("operations") which can be run concurrently
                         max_concurrent_work = max(grouping, m*k, k*n, m*n, cmax*rmax)
 
-                        # Miniumum number of threads required to have one thread per tile
+                        # Minimum number of threads required to have one thread per tile
                         # i.e., cover the result matrix
                         min_threads = cmax * rmax
 
                         # Shared memory buffer size
                         buf_sz = max(m*n, m*k + k*tn*cmax, tm*rmax*k + 1)
                         smem_tot = buf_sz * cu.sizeof_double + cu.npar * grouping * cu.sizeof_int
-                        if (smem_tot > gpu["SMEMperBLOCK"]):
+                        if smem_tot > gpu["SMEMperBLOCK"]:
                             continue
-                        if (smem_tot * minblocks > gpu["SMEMperSM"]):
+                        if smem_tot * minblocks > gpu["SMEMperSM"]:
                             continue
 
                         # Use all concurrency available: fill warps
@@ -72,12 +72,12 @@ class Kernel_dnt_medium(cusmm_dnt.Kernel):
                             if threads < min_threads:
                                 continue
 
-                            params.append({'m':m, 'n':n, 'k':k,
-                                           'tile_m':tm, 'tile_n':tn,
-                                           'threads':threads,
-                                           'grouping':grouping,
-                                           'minblocks':minblocks})
-        return(params)
+                            params.append({'m': m, 'n': n, 'k': k,
+                                           'tile_m': tm, 'tile_n': tn,
+                                           'threads': threads,
+                                           'grouping': grouping,
+                                           'minblocks': minblocks})
+        return params
 
 
 #EOF
