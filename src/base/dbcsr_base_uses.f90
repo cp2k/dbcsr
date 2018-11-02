dc17ef00436d8a00f88feea7f2d2e43a2f43b47c
Hans Pabst <hfp@users.noreply.github.com>
2018-11-02 14:21:18 +0100

    Joaat hash to produce CRC32 hashes (#74)
    Call libxsmm_hash to implement joaat_hash. This function produces CRC32 hashes and leverages specific CPU instructions. The CRC32 hash may also improve the hash quality and distribution. LIBXSMM provides this function since v1.9.0-6 as part of the Fortran interface (tested in CP2K/intel fork since then).
    
    The LIBXSMM based implementation is not yet active since even the master revision is below the version-requirement (v1.10). LIBXSMM v.1.10 will be the first release with the Fortran interface for libxsmm_hash.

--- src/base/dbcsr_base_uses.f90
+++ src/base/dbcsr_base_uses.f90
@@ -36,3 +36,9 @@
 ! Calculate version number from 3-components. Can be used for comparison e.g.,
 ! TO_VERSION(4, 9, 0) <= TO_VERSION(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)
 #define TO_VERSION(MAJOR, MINOR, UPDATE) ((MAJOR) * 10000 + (MINOR) * 100 + (UPDATE))
+
+! LIBXSMM has a FORTRAN-suitable header with macro/version definitions (since v1.8.2).
+! Allows macro-toggles (in addition to parameters).
+#if defined(__LIBXSMM)
+#include <libxsmm_config.h>
+#endif
