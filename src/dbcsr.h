e82016c0172a345cb6c838b0d3d41099778d59ae
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-11-05 09:56:01 +0100

    dbcsr.h: make functions static, drop C++ aliases
    Following the discussion on #65 and in consideration of PR #64 which
    adds a fully OO C++ interface, I consider it to be safer to drop the
    aliased C++ functions for now to avoid that people start relying on an
    interface we are going to change later on.
    
    fixes: #65

--- src/dbcsr.h
+++ src/dbcsr.h
@@ -2,7 +2,7 @@
 #define DBCSR_H
 
 #include <mpi.h>
-#include <stdbool.h> /* we need bool introduce in C99 */
+#include <stdbool.h> /* we need bool from C99 */
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,7 +11,7 @@ extern "C" {
     
     void c_dbcsr_finalize_lib_aux(MPI_Fint* fcomm);
     
-    void c_dbcsr_finalize_lib(MPI_Comm comm)
+    static void c_dbcsr_finalize_lib(MPI_Comm comm)
     {
         MPI_Fint fcomm = MPI_Comm_c2f(comm);
         c_dbcsr_finalize_lib_aux(&fcomm);
@@ -20,8 +20,8 @@ extern "C" {
     void c_dbcsr_distribution_new_aux(void** dist, MPI_Fint* fcomm, int* row_dist, int row_dist_size,
                                       int* col_dist, int col_dist_size);
     
-    void c_dbcsr_distribution_new(void** dist, MPI_Comm comm, int* row_dist, int row_dist_size,
-                                              int* col_dist, int col_dist_size)
+    static void c_dbcsr_distribution_new(void** dist, MPI_Comm comm, int* row_dist, int row_dist_size,
+                                         int* col_dist, int col_dist_size)
     {
         MPI_Fint fcomm = MPI_Comm_c2f(comm);
         c_dbcsr_distribution_new_aux(dist, &fcomm, row_dist, row_dist_size, col_dist, col_dist_size);
@@ -31,7 +31,6 @@ extern "C" {
 
     void c_dbcsr_create_new_d(void** matrix, const char* name, void* dist, char matrix_type, int* row_blk_sizes,
                               int row_blk_sizes_length, int* col_blk_sizes, int col_blk_sizes_length);
-
 
     void c_dbcsr_finalize(void* matrix);
     
@@ -47,23 +46,6 @@ extern "C" {
                             double beta, void** c_matrix_c, bool* retain_sparsity);
 #ifdef __cplusplus
 }
-
-
-namespace dbcsr {
-    constexpr auto& init_lib = c_dbcsr_init_lib;
-    constexpr auto& finalize_lib = c_dbcsr_finalize_lib;
-    constexpr auto& distribution_new = c_dbcsr_distribution_new;
-    constexpr auto& distribution_release = c_dbcsr_distribution_release;
-    constexpr auto& create_new_d = c_dbcsr_create_new_d;
-    constexpr auto& finalize = c_dbcsr_finalize;
-    constexpr auto& release = c_dbcsr_release;
-    constexpr auto& print = c_dbcsr_print;
-    constexpr auto& get_stored_coordinates = c_dbcsr_get_stored_coordinates;
-    constexpr auto& put_block_d = c_dbcsr_put_block_d;
-    constexpr auto& multiply_d = c_dbcsr_multiply_d;
-}
-
 #endif
-
 
 #endif // DBCSR_H
