f977193475a67ade710fc04bdd4f362e95604da8
shoshijak <as.jak@protonmail.ch>
2018-11-27 13:37:10 +0100

    libcusmm/kernels: indent generated autotuning code

--- src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt.py
+++ src/acc/libsmm_acc/libcusmm/kernels/cusmm_dnt.py
@@ -58,21 +58,21 @@ class Kernel:
         output  = "int launch_" + self.name + "(int *param_stack, int stack_size, "
         output += "cudaStream_t stream, int m_max, int n_max, int k_max, "
         output += "double *a_data, double *b_data, double *c_data){\n"
-        output += "int shared_size = 0;\n"
-        output += "//%s\n" % str(self.__dict__)
-        output += "typedef void (*kernel)(const int*, int, const double*, const double*, double*);\n"
-        output += "static kernel kern_func = " + self.func_signature
-        output += "static bool configured = false;\n"
-        output += "if(configured == false){\n"
-        output += "  cudaError_t err = cudaFuncSetSharedMemConfig(kern_func, cudaSharedMemBankSizeEightByte);\n"
-        output += "  if(err != cudaSuccess) return(-1);\n"
-        output += "  configured = true;\n"
-        output += "}\n"
-        output += "kern_func<<< ((stack_size + %(grouping)d - 1) / %(grouping)d), %(threads)d, shared_size, stream >>>\n" \
+        output += "  int shared_size = 0;\n"
+        output += "  //%s\n" % str(self.__dict__)
+        output += "  typedef void (*kernel)(const int*, int, const double*, const double*, double*);\n"
+        output += "  static kernel kern_func = " + self.func_signature
+        output += "  static bool configured = false;\n"
+        output += "  if(configured == false){\n"
+        output += "    cudaError_t err = cudaFuncSetSharedMemConfig(kern_func, cudaSharedMemBankSizeEightByte);\n"
+        output += "    if(err != cudaSuccess) return(-1);\n"
+        output += "    configured = true;\n"
+        output += "  }\n"
+        output += "  kern_func<<< ((stack_size + %(grouping)d - 1) / %(grouping)d), %(threads)d, shared_size, stream >>>\n" \
                   % self.__dict__
-        output += "(param_stack, stack_size, \n"
-        output += "a_data, b_data, c_data);\n"
-        output += "return(0);\n"
+        output += "  (param_stack, stack_size, \n"
+        output += "  a_data, b_data, c_data);\n"
+        output += "  return(0);\n"
         output += "}\n"
         return output
 
