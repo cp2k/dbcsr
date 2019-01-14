227b76f650646ee2c2c9414d5ee3dcbe979873ad
shoshijak <as.jak@protonmail.ch>
2019-01-14 09:23:13 +0100

    libcusmm/predict/collect: add commands to merge CSV files
    - And refactor

--- src/acc/libsmm_acc/libcusmm/predict_collect.py
+++ src/acc/libsmm_acc/libcusmm/predict_collect.py
@@ -90,6 +90,118 @@ def main():
 
     # ===============================================================================
     # Collect information and write to csv
+    collect_training_data(
+        kernel_folders,
+        kernel_folder_pattern,
+        gpu_properties,
+        autotuning_properties,
+        max_performances_per_mnk,
+        max_performances_per_algo_per_mnk,
+        baseline_performances_per_algo_per_mnk,
+    )
+
+    # ===============================================================================
+    # Print max performance dictionaries
+    max_performances_per_mnk_file = os.path.join(options.folder, "max_performances.json")
+    with open(max_performances_per_mnk_file, "w") as f:
+        json.dump(max_performances_per_mnk, f)
+    max_performances_per_algo_per_mnk_file = os.path.join(options.folder, "max_performances_by_algo.json")
+    with open(max_performances_per_algo_per_mnk_file, "w") as f:
+        json.dump(max_performances_per_algo_per_mnk, f)
+    baseline_performances_per_algo_per_mnk_file = os.path.join(options.folder, "baseline_performances_by_algo.json")
+    with open(baseline_performances_per_algo_per_mnk_file, "w") as f:
+        json.dump(baseline_performances_per_algo_per_mnk, f)
+    print(
+        "\nWrote max. and baseline performances to:\n",
+        max_performances_per_mnk_file,
+        ",\n",
+        max_performances_per_algo_per_mnk_file,
+        " and\n",
+        baseline_performances_per_algo_per_mnk_file,
+    )
+
+    # ===============================================================================
+    # Print commands to merge CSVs into one big CSV for training data
+    print("Merge all individual CSV files into one by running the following commands:\n")
+    print_merging_commands(kernel_folders, kernel_folder_pattern)
+
+
+# ===============================================================================
+# Helper variables and functions (formatting & writing)
+autotuning_line = re.compile(
+    r"OK Kernel_dnt_(\w+) m (\d+)\s+n (\d+)\s+k (\d+)\s+"
+    + r"(?:tile_m (\d+)\s+tile_n (\d+)\s+(?:w (\d+)\s+v (\d+)\s+)?)?"
+    + r"threads (\d+)\s+grouping (\d+)\s+minblocks (\d+)\s+GFlop/s (\d+(?:\.\d+)?)"
+)
+
+
+def read_log_file(log_folder, m, n, k):
+    """
+    Given a folder of kernel autotuning, read and parse the autotuning information in the log file
+    and return it in the form of a pandas Dataframe.
+    :param log_folder: folder of kernel autotuning
+    :return: pandas Dataframe containing autotuning information
+    """
+    # Find log files in the log folder
+    log_files = [f for f in os.listdir(log_folder) if f[-4:] == ".log"]
+    assert len(log_files) > 0
+    log_files = sorted(log_files)
+    print("Found log files:", log_files)
+
+    # Parse the log files and collect data
+    data = list()
+    for log_file in log_files:
+
+        print("Processing log file", log_file)
+        with open(os.path.join(log_folder, log_file), "r") as f:
+            log_file_content = f.read().splitlines()
+
+        for l in log_file_content:
+
+            if "OK" in l:  # this line contains autotuning data
+
+                # Parse the line
+                match = autotuning_line.match(l)
+                assert match is not None, "Found null match: " + l
+
+                # Get algorithm, parameters, and performance
+                data.append(
+                    {
+                        "m": m,
+                        "n": n,
+                        "k": k,
+                        "algorithm": match.group(1),
+                        "threads_per_blk": int(match.group(9)),
+                        "grouping": int(match.group(10)),
+                        "minblocks": int(match.group(11)),
+                        "tile_m": int(match.group(5)) if match.group(5) is not None else None,
+                        "tile_n": int(match.group(6)) if match.group(6) is not None else None,
+                        "w": int(match.group(7)) if match.group(7) is not None else None,
+                        "v": int(match.group(8)) if match.group(8) is not None else None,
+                        "perf (Gflop/s)": float(match.group(12)),
+                    }
+                )
+
+    print("Autotuning lines found: ", len(data))
+
+    # Merge dictionaries into a pandas dataframe
+    dataframe = pd.DataFrame(data)
+
+    return dataframe
+
+
+def collect_training_data(
+    kernel_folders,
+    kernel_folder_pattern,
+    gpu_properties,
+    autotuning_properties,
+    max_performances_per_mnk,
+    max_performances_per_algo_per_mnk,
+    baseline_performances_per_algo_per_mnk,
+):
+
+    n_kernels = len(kernel_folders)
+
     # For each folder:
     for i, kernel_folder in enumerate(kernel_folders):
 
@@ -167,89 +279,52 @@ def main():
                     data_algo[pars_to_get].to_csv(derived_parameters_file_name)
                     print("\tWrote", derived_parameters_file_name)
 
-    # ===============================================================================
-    # Print max performance dictionaries
-    max_performances_per_mnk_file = os.path.join(options.folder, "max_performances.json")
-    with open(max_performances_per_mnk_file, "w") as f:
-        json.dump(max_performances_per_mnk, f)
-    max_performances_per_algo_per_mnk_file = os.path.join(options.folder, "max_performances_by_algo.json")
-    with open(max_performances_per_algo_per_mnk_file, "w") as f:
-        json.dump(max_performances_per_algo_per_mnk, f)
-    baseline_performances_per_algo_per_mnk_file = os.path.join(options.folder, "baseline_performances_by_algo.json")
-    with open(baseline_performances_per_algo_per_mnk_file, "w") as f:
-        json.dump(baseline_performances_per_algo_per_mnk, f)
-    print(
-        "\nWrote max. and baseline performances to:\n",
-        max_performances_per_mnk_file,
-        ",\n",
-        max_performances_per_algo_per_mnk_file,
-        " and\n",
-        baseline_performances_per_algo_per_mnk_file,
-    )
-
 
 # ===============================================================================
-# Helper variables and functions (formatting & writing)
-autotuning_line = re.compile(
-    r"OK Kernel_dnt_(\w+) m (\d+)\s+n (\d+)\s+k (\d+)\s+"
-    + r"(?:tile_m (\d+)\s+tile_n (\d+)\s+(?:w (\d+)\s+v (\d+)\s+)?)?"
-    + r"threads (\d+)\s+grouping (\d+)\s+minblocks (\d+)\s+GFlop/s (\d+(?:\.\d+)?)"
-)
+def print_merging_commands(kernel_folders, kernel_folder_pattern):
+    for algorithm in kernel_algorithm.keys():
+        for data_type in ("raw_", ""):
 
+            print("$ # Merge instructions for algorithm", algorithm)
+            training_data_file = "{data_type}training_data_{algorithm}.csv".format(
+                data_type=data_type, algorithm=algorithm
+            )
 
-def read_log_file(log_folder, m, n, k):
-    """
-    Given a folder of kernel autotuning, read and parse the autotuning information in the log file
-    and return it in the form of a pandas Dataframe.
-    :param log_folder: folder of kernel autotuning
-    :return: pandas Dataframe containing autotuning information
-    """
-    # Find log files in the log folder
-    log_files = [f for f in os.listdir(log_folder) if f[-4:] == ".log"]
-    assert len(log_files) > 0
-    log_files = sorted(log_files)
-    print("Found log files:", log_files)
+            if os.path.exists(training_data_file):
+                print("$ # Found {}, append new training data to this file:".format(training_data_file))
 
-    # Parse the log files and collect data
-    data = list()
-    for log_file in log_files:
+            else:
 
-        print("Processing log file", log_file)
-        with open(os.path.join(log_folder, log_file), "r") as f:
-            log_file_content = f.read().splitlines()
+                # Find an (m, n, k) for this algorithm to get its header line
+                for i, kernel_folder in enumerate(kernel_folders):
 
-        for l in log_file_content:
+                    # Find (m, n, k)
+                    match = kernel_folder_pattern.search(kernel_folder).groups()
+                    m = int(match[0])
+                    n = int(match[1])
+                    k = int(match[2])
 
-            if "OK" in l:  # this line contains autotuning data
+                    file_name = os.path.join(
+                        kernel_folder,
+                        "{data_type}training_data_{mnk}_{algorithm}.csv".format(
+                            data_type=data_type, mnk=to_string(m, n, k), algorithm=algorithm
+                        ),
+                    )
+                    if os.path.exists(file_name):
+                        print(
+                            "$ head -1 {base_file} > {training_data_file}".format(
+                                base_file=file_name, training_data_file=training_data_file
+                            )
+                        )
+                        break
+                else:
+                    print("Did not find any existing files for algorithm", algorithm, "and data type", data_type)
 
-                # Parse the line
-                match = autotuning_line.match(l)
-                assert match is not None, "Found null match: " + l
-
-                # Get algorithm, parameters, and performance
-                data.append(
-                    {
-                        "m": m,
-                        "n": n,
-                        "k": k,
-                        "algorithm": match.group(1),
-                        "threads_per_blk": int(match.group(9)),
-                        "grouping": int(match.group(10)),
-                        "minblocks": int(match.group(11)),
-                        "tile_m": int(match.group(5)) if match.group(5) is not None else None,
-                        "tile_n": int(match.group(6)) if match.group(6) is not None else None,
-                        "w": int(match.group(7)) if match.group(7) is not None else None,
-                        "v": int(match.group(8)) if match.group(8) is not None else None,
-                        "perf (Gflop/s)": float(match.group(12)),
-                    }
+            print(
+                "$ tail -n +2 -q tune_*/raw_training_data_*_{algorithm}.csv >> {training_data_file}".format(
+                    algorithm=algorithm, training_data_file=training_data_file
                 )
-
-    print("Autotuning lines found: ", len(data))
-
-    # Merge dictionaries into a pandas dataframe
-    dataframe = pd.DataFrame(data)
-
-    return dataframe
+            )
 
 
 # ===============================================================================
