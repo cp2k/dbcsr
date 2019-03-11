0da4fdb79241c7b878b9314ae06cec1492de6e82
shoshijak <as.jak@protonmail.ch>
2019-03-11 11:07:05 +0100

    check_header: open files as bytes stream

--- .pre-commit/check_header.py
+++ .pre-commit/check_header.py
@@ -44,10 +44,8 @@ def check_header(header_dir, files, verbose=False):
     header_len = {}
 
     for headertype in TYPES:
-        with open(path.join(header_dir, headertype), 'r') as fhandle:
+        with open(path.join(header_dir, headertype), 'rb') as fhandle:
             header_content = fhandle.read()
-            if not hasattr(header_content, "decode"):
-                header_content = bytes(header_content, 'utf-8')
             header_re[headertype] = re.compile(re.escape(header_content))
             header_len[headertype] = len(header_content)
 
