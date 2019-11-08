057fbd75a3c8a67c3c9714c01d5a7da85d7cf916
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-11-08 11:21:32 +0100

    ci/daint: update cmake to 3.14.5

--- .ci/daint.cscs.ch/gnu.test.sh
+++ .ci/daint.cscs.ch/gnu.test.sh
@@ -14,7 +14,7 @@ set -o nounset
 set -o pipefail
 
 module swap PrgEnv-cray PrgEnv-gnu
-module load daint-gpu cudatoolkit CMake/3.12.0
+module load daint-gpu cudatoolkit CMake/3.14.5
 module unload cray-libsci_acc
 module list
 
