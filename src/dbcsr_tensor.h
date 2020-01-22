8492a9ecedb38d425f1c34d295abd285129bea57
ambmax00 <maximilien.ambroise@gmail.com>
2020-01-22 12:23:16 +0100

    Make intent(in) variables const in header file fir tensor C API.

--- src/dbcsr_tensor.h
+++ src/dbcsr_tensor.h
@@ -11,96 +11,108 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    void c_dbcsr_t_pgrid_create(MPI_Fint* fcomm, int* c_dims, int dims_size, void** c_pgrid, int* c_tensor_dims,
-		int* c_map1_2d, int map1_2d_size, int* c_map2_2d, int map2_2d_size, int* nsplit, int* dimsplit);
+    void c_dbcsr_t_pgrid_create(const MPI_Fint* fcomm, int* c_dims, const int dims_size, 
+		void** c_pgrid, int* c_tensor_dims, const int* c_map1_2d, const int map1_2d_size, 
+		const int* c_map2_2d, const int map2_2d_size, const int* nsplit, const int* dimsplit);
 	
-	void c_dbcsr_t_pgrid_destroy(void** c_pgrid, bool* c_keep_comm);
+	void c_dbcsr_t_pgrid_destroy(void** c_pgrid, const bool* c_keep_comm);
 																			  
-	void c_dbcsr_t_distribution_new(void** c_dist, void* c_pgrid, int* c_map1_2d, int map1_2d_size,
-		int* c_map2_2d, int map2_2d_size, ${extern_varlist_and_size("c_nd_dist")}$, bool* own_comm);
+	void c_dbcsr_t_distribution_new(void** c_dist, const void* c_pgrid, const int* c_map1_2d, 
+		const int map1_2d_size, const int* c_map2_2d, const int map2_2d_size, 
+		${extern_varlist_and_size("c_nd_dist", "const int")}$, const bool* own_comm);
 		
     void c_dbcsr_t_distribution_destroy(void** c_dist);
 		
-	void c_dbcsr_t_create_new(void** c_tensor, const char* c_name, void* c_dist, int* c_map1_2d, int c_map1_2d_size,
-       int* c_map2_2d, int c_map2_2d_size, int* data_type, ${extern_varlist_and_size("c_blk_size")}$);
+	void c_dbcsr_t_create_new(void** c_tensor, const char* c_name, const void* c_dist, 
+		const int* c_map1_2d, const int c_map1_2d_size, const int* c_map2_2d, 
+		const int c_map2_2d_size, const int* data_type, ${extern_varlist_and_size("c_blk_size", "const int")}$);
        
-    void c_dbcsr_t_create_template(void* c_tensor_in, void** c_tensor, const char* c_name);
+    void c_dbcsr_t_create_template(const void* c_tensor_in, void** c_tensor, const char* c_name);
     
-    void c_dbcsr_t_create_matrix(void* c_matrix_in, void** c_tensor, int* c_order, const char* c_name);
+    void c_dbcsr_t_create_matrix(const void* c_matrix_in, void** c_tensor, const int* c_order, const char* c_name);
        
     void c_dbcsr_t_destroy(void** c_tensor);
                                
-    int c_ndims_tensor(void* tensor);
+    int c_ndims_tensor(const void* c_tensor);
                                
 #:for dsuffix, ctype in c_dtype_float_list
 #:for ndim in ndims
 
-	  void c_dbcsr_t_allocate_and_get_${ndim}$d_block_${dsuffix}$ (void* c_tensor, int tensor_dim, int* c_ind, 
+	  void c_dbcsr_t_allocate_and_get_${ndim}$d_block_${dsuffix}$ (const void* c_tensor, const int tensor_dim, const int* c_ind, 
 		${ctype}$** c_block, bool* c_found);
                     
-      void c_dbcsr_t_get_${ndim}$d_block_${dsuffix}$ (void* c_tensor, int tensor_dim, int* c_ind, int* c_sizes, 
+      void c_dbcsr_t_get_${ndim}$d_block_${dsuffix}$ (const void* c_tensor, const int tensor_dim, const int* c_ind, const int* c_sizes, 
 		${ctype}$* c_block, bool* c_found);
 		
-	  void c_dbcsr_t_put_${ndim}$d_block_${dsuffix}$ (void* c_tensor, int tensor_dim, int* c_ind, int*c_sizes, 
-        ${ctype}$* c_block, bool* c_summation, ${ctype}$* c_scale);
+	  void c_dbcsr_t_put_${ndim}$d_block_${dsuffix}$ (const void* c_tensor, const int tensor_dim, const int* c_ind, const int*c_sizes, 
+        const ${ctype}$* c_block, const bool* c_summation, const ${ctype}$* c_scale);
 
 #:endfor
      
-     void c_dbcsr_t_contract_${dsuffix}$ (${ctype}$ c_alpha, void* c_tensor_1, void* c_tensor_2, ${ctype}$ c_beta, void* c_tensor_3, 
-                               int* c_contract_1, int c_contract_1_size,
-                               int* c_notcontract_1, int c_notcontract_1_size,
-                               int* c_contract_2, int c_contract_2_size,
-                               int* c_notcontract_2, int c_notcontract_2_size,
-                               int* c_map_1, int c_map_1_size,
-                               int* c_map_2, int c_map_2_size,
-                               int* c_bounds_1, 
-                               int* c_bounds_2, 
-                               int* c_bounds_3, 
-                               bool* c_optimize_dist, 
-                               void** c_pgrid_opt_1, 
-                               void** c_pgrid_opt_2, 
-                               void** c_pgrid_opt_3,
-                               double* filter_eps, long long int* flop, 
-                               bool* move_data, int* unit_nr, bool* log_verbose);
+     void c_dbcsr_t_contract_${dsuffix}$ (	
+			const ${ctype}$ c_alpha, 
+			void* c_tensor_1, 
+			void* c_tensor_2, 
+			const ${ctype}$ c_beta, 
+			void* c_tensor_3, 
+			const int* c_contract_1, const int c_contract_1_size,
+            const int* c_notcontract_1, const int c_notcontract_1_size,
+            const int* c_contract_2, const int c_contract_2_size,
+            const int* c_notcontract_2, const int c_notcontract_2_size,
+            const int* c_map_1, const int c_map_1_size,
+            const int* c_map_2, const int c_map_2_size,
+            const int* c_bounds_1, 
+            const int* c_bounds_2, 
+            const int* c_bounds_3, 
+            const bool* c_optimize_dist, 
+            void** c_pgrid_opt_1, 
+            void** c_pgrid_opt_2, 
+            void** c_pgrid_opt_3,
+            const double* filter_eps, 
+            long long int* flop, 
+            const bool* move_data, 
+            const int* unit_nr, 
+            const bool* log_verbose);
      
-     void c_dbcsr_t_filter_${dsuffix}$ (void* c_tensor, ${ctype}$ c_eps, int* c_method, bool* c_use_absolute);
+     void c_dbcsr_t_filter_${dsuffix}$ (const void* c_tensor, const ${ctype}$ c_eps, 
+		const int* c_method, const bool* c_use_absolute);
      
-     void c_dbcsr_t_set_${dsuffix}$ (void* c_tensor, ${ctype}$ c_alpha);
+     void c_dbcsr_t_set_${dsuffix}$ (const void* c_tensor, const ${ctype}$ c_alpha);
 	 
-	 void c_dbcsr_t_scale_${dsuffix}$ (void* c_tensor, ${ctype}$ c_alpha);
+	 void c_dbcsr_t_scale_${dsuffix}$ (const void* c_tensor, const ${ctype}$ c_alpha);
 	
 #:endfor
  
 
-     void c_dbcsr_t_get_stored_coordinates(void* c_tensor, int tensor_dim, int* c_ind_nd, int* c_processor);
+     void c_dbcsr_t_get_stored_coordinates(const void* c_tensor, const int tensor_dim, int* c_ind_nd, int* c_processor);
    
-     void c_dbcsr_t_reserve_blocks_index(void* c_tensor, int nblocks, ${varlist("int* c_blk_ind")}$);
+     void c_dbcsr_t_reserve_blocks_index(const void* c_tensor, const int nblocks, ${varlist("const int* c_blk_ind")}$);
      
-     int c_ndims_iterator(void* c_iterator); 
+     int c_ndims_iterator(const void* c_iterator); 
      
-	 void c_dbcsr_t_iterator_start(void** c_iterator, void* c_tensor);
+	 void c_dbcsr_t_iterator_start(void** c_iterator, const void* c_tensor);
 
      void c_dbcsr_t_iterator_stop(void** c_iterator);
    
-     void c_dbcsr_t_iterator_next_block(void* c_iterator, int iterator_size, int* c_ind_nd, 
+     void c_dbcsr_t_iterator_next_block(const void* c_iterator, const int iterator_size, int* c_ind_nd, 
           int* c_blk, int* c_blk_p, int* c_blk_size, int* c_blk_offset);
           
-     bool c_dbcsr_t_iterator_blocks_left(void* c_iterator);
+     bool c_dbcsr_t_iterator_blocks_left(const void* c_iterator);
                                
-     void c_dbcsr_t_split_blocks(void* c_tensor_in, int tensor_dim, void** c_tensor_out, int* c_block_sizes, bool* c_nodata);
+     void c_dbcsr_t_split_blocks(const void* c_tensor_in, const int tensor_dim, void** c_tensor_out, const int* c_block_sizes, const bool* c_nodata);
      
-     void c_dbcsr_t_copy_matrix_to_tensor(void* c_matrix_in, void* c_tensor_out, bool* c_summation);
+     void c_dbcsr_t_copy_matrix_to_tensor(const void* c_matrix_in, void* c_tensor_out, const bool* c_summation);
      
-     void c_dbcsr_t_copy_tensor_to_matrix(void* c_tensor_in, void* c_matrix_out, bool* c_summation);
+     void c_dbcsr_t_copy_tensor_to_matrix(const void* c_tensor_in, void* c_matrix_out, const bool* c_summation);
      
-     void c_dbcsr_t_blk_sizes(void* c_tensor, int tensor_dim, int* c_ind, int* c_blk_size);
+     void c_dbcsr_t_blk_sizes(const void* c_tensor, const int tensor_dim, const int* c_ind, int* c_blk_size);
      
-     void c_dbcsr_t_copy(void* c_tensor_in, int tensor_dim, void* c_tensor_out, int* c_order, bool* c_summation, 
-      bool* c_move_data, int* c_unit_nr);
+     void c_dbcsr_t_copy(const void* c_tensor_in, const int tensor_dim, void* c_tensor_out, const int* c_order, const bool* c_summation, 
+      const bool* c_move_data, const int* c_unit_nr);
       
      void c_dbcsr_t_clear(void* c_tensor);
      
-     void c_dbcsr_t_get_info(void* c_tensor, int tensor_dim, 
+     void c_dbcsr_t_get_info(const void* c_tensor, const int tensor_dim, 
 							   int* c_nblks_total,
                                int* c_nfull_total,
                                int* c_nblks_local,
@@ -115,18 +127,18 @@ extern "C" {
                                char** name, int* name_size,
                                int* data_type); 
       
-     void c_dbcsr_t_get_nd_index_blk(void* c_tensor, void** c_nd_index_blk);
+     void c_dbcsr_t_get_nd_index_blk(const void* c_tensor, void** c_nd_index_blk);
      
-     void c_dbcsr_t_get_nd_index(void* c_tensor, void** c_nd_index);
+     void c_dbcsr_t_get_nd_index(const void* c_tensor, void** c_nd_index);
                             
-     void c_dbcsr_t_get_mapping_info(void* c_map, int* ndim_nd, int* ndim1_2d, int* ndim2_2d, 
+     void c_dbcsr_t_get_mapping_info(const void* c_map, int* ndim_nd, int* ndim1_2d, int* ndim2_2d, 
                         long long int* c_dims_2d_i8, int* c_dims_2d, int** c_dims_nd, int* dims_nd_size, 
                         int** c_dims1_2d, int* dims1_2d_size, int** c_dims2_2d, int* dims2_2d_size, 
                         int** c_map1_2d, int* map1_2d_size, int** c_map2_2d, int* map2_2d_size, 
                         int** c_map_nd, int* map_nd_size, int* base, bool* c_col_major);
                         
-     int c_dbcsr_t_get_num_blocks(void* c_tensor); 
-     long long int c_dbcsr_t_get_num_blocks_total(void* c_tensor);
+     int c_dbcsr_t_get_num_blocks(const void* c_tensor); 
+     long long int c_dbcsr_t_get_num_blocks_total(const void* c_tensor);
 	
 #ifdef __cplusplus
 }
@@ -139,7 +151,7 @@ extern "C" {
  * *************************************************************************************************/
 
 #:for dsuffix, ctype in c_dtype_float_list
-static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, int* c_sizes, 
+static void c_dbcsr_t_get_block(const void* c_tensor, const int* c_ind, const int* c_sizes, 
 		${ctype}$* c_block, bool* c_found) {
 	
 	int tensor_dim = c_ndims_tensor(c_tensor);
@@ -156,7 +168,7 @@ static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, int* c_sizes,
 #:endfor
 
 #:for dsuffix, ctype in c_dtype_float_list
-static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind, 
+static void c_dbcsr_t_get_block(const void* c_tensor, const int* c_ind, 
 		${ctype}$** c_block, bool* c_found) {
 	
 	int tensor_dim = c_ndims_tensor(c_tensor);
@@ -173,8 +185,8 @@ static void c_dbcsr_t_get_block(void* c_tensor, int* c_ind,
 #:endfor
 
 #:for dsuffix, ctype in c_dtype_float_list
-static void c_dbcsr_t_put_block(void* c_tensor, int* c_ind, int* c_sizes, 
-		${ctype}$* c_block, bool* c_summation, ${ctype}$* c_scale) {
+static void c_dbcsr_t_put_block(const void* c_tensor, const int* c_ind, const int* c_sizes, 
+		const ${ctype}$* c_block, const bool* c_summation, const ${ctype}$* c_scale) {
 	
 	int tensor_dim = c_ndims_tensor(c_tensor);
 	
@@ -189,30 +201,14 @@ static void c_dbcsr_t_put_block(void* c_tensor, int* c_ind, int* c_sizes,
 }
 #:endfor
 
-static void c_dbcsr_t_get_stored_coordinates(void* c_tensor, int* c_ind_nd, int* c_processor) {
+static void c_dbcsr_t_get_stored_coordinates(const void* c_tensor, int* c_ind_nd, int* c_processor) {
 	
 	int tensor_dim = c_ndims_tensor(c_tensor);
 	c_dbcsr_t_get_stored_coordinates(c_tensor, tensor_dim, c_ind_nd, c_processor);
 	
 }
 
-/*
-#:for ndim in range(2, maxrank)
-static void c_dbcsr_t_reserve_blocks_index(void* c_tensor, ${extern_varlist_and_size("c_blk_ind", nmax=ndim)}$) {
-	
-	c_dbcsr_t_reserve_blocks_index(c_tensor, ${c_varlist_and_size("c_blk_ind", nmax=ndim)}$, 
-#:for n in range(ndim+1,maxrank+1)
-  #:if n == maxrank
-    nullptr, 0);
-  #:else
-    nullptr, 0,
-  #:endif
-#:endfor
-};
-#:endfor
-*/
-
-static void c_dbcsr_t_iterator_next_block(void* c_iterator, int* c_ind_nd, 
+static void c_dbcsr_t_iterator_next_block(const void* c_iterator, int* c_ind_nd, 
           int* c_blk, int* c_blk_p, int* c_blk_size, int* c_blk_offset) {
 			  
 	int iterator_size = c_ndims_iterator(c_iterator);
@@ -223,19 +219,19 @@ static void c_dbcsr_t_iterator_next_block(void* c_iterator, int* c_ind_nd,
 }
 
 #:for dsuffix, ctype in c_dtype_float_list
-static void c_dbcsr_t_filter(void* c_tensor, ${ctype}$ c_eps, int* c_method, bool* c_use_absolute) {
+static void c_dbcsr_t_filter(const void* c_tensor, const ${ctype}$ c_eps, const int* c_method, const bool* c_use_absolute) {
 	
 	c_dbcsr_t_filter_${dsuffix}$ (c_tensor, c_eps, c_method, c_use_absolute);
 	
 }
 
-static void c_dbcsr_t_set(void* c_tensor, ${ctype}$ c_alpha) {
+static void c_dbcsr_t_set(const void* c_tensor, const ${ctype}$ c_alpha) {
    
    c_dbcsr_t_set_${dsuffix}$ (c_tensor, c_alpha);
    
 }
 
-static void c_dbcsr_t_scale(void* c_tensor, ${ctype}$ c_alpha) {
+static void c_dbcsr_t_scale(const void* c_tensor, const ${ctype}$ c_alpha) {
 	
    c_dbcsr_t_scale_${dsuffix}$ (c_tensor, c_alpha);
    
@@ -243,7 +239,7 @@ static void c_dbcsr_t_scale(void* c_tensor, ${ctype}$ c_alpha) {
 
 #:endfor
 
-static void c_dbcsr_t_blk_sizes(void* c_tensor, int* c_ind, int* c_blk_size) {
+static void c_dbcsr_t_blk_sizes(const void* c_tensor, const int* c_ind, int* c_blk_size) {
 	
 	int tensor_dim = c_ndims_tensor(c_tensor);
 	c_dbcsr_t_blk_sizes(c_tensor, tensor_dim, c_ind, c_blk_size);
