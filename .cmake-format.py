f46cce63f185f7153c5b4968f08a2848b83301b1
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2020-10-06 14:39:01 +0200

    pre-commit: format cmake-files

---
+++ .cmake-format.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+with section("format"):
+    separate_ctrl_name_with_space = True
--- cmake/CheckCompilerSupport.cmake
+++ cmake/CheckCompilerSupport.cmake
@@ -1,24 +1,23 @@
-
 include(CheckFortranSourceCompiles)
 
-set(CHECK_PROGRAMS
-  f2008-norm2.f90
-  f2008-block_construct.f90
-  f2008-contiguous.f90
-  )
+set(CHECK_PROGRAMS f2008-norm2.f90 f2008-block_construct.f90
+                   f2008-contiguous.f90)
 
 if (CMAKE_Fortran_COMPILER_ID STREQUAL "PGI")
   set(CHECK_PROGRAMS ${CHECK_PROGRAMS} f95-reshape-order-allocatable.f90)
 endif ()
 
 foreach (prog ${CHECK_PROGRAMS})
-  get_filename_component(prog_ext ${prog} EXT)  # get the src extension to pass along
+  get_filename_component(prog_ext ${prog} EXT) # get the src extension to pass
+                                               # along
   get_filename_component(prog_name ${prog} NAME_WE)
 
   file(READ "${CMAKE_CURRENT_LIST_DIR}/compiler-tests/${prog}" prog_src)
-  check_fortran_source_compiles("${prog_src}" "${prog_name}" SRC_EXT "${prog_ext}")
+  CHECK_Fortran_SOURCE_COMPILES("${prog_src}" "${prog_name}" SRC_EXT
+                                "${prog_ext}")
 
   if (NOT ${prog_name})
-    message(FATAL_ERROR "Your compiler does not support all required F2008 features")
+    message(
+      FATAL_ERROR "Your compiler does not support all required F2008 features")
   endif ()
 endforeach ()
