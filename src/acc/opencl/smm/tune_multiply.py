55aef80f224514434220b4940eacd24699f4948c
Hans Pabst <hans.pabst@intel.com>
2021-01-15 20:09:08 +0100

    Fixed file header/banner.

--- src/acc/opencl/smm/tune_multiply.py
+++ src/acc/opencl/smm/tune_multiply.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-###############################################################################
-# Copyright (C) by the DBCSR developers group - All rights reserved           #
-# This file is part of the DBCSR library.                                     #
-#                                                                             #
-# For information on the license, see the LICENSE file.                       #
-# For further information please visit https://dbcsr.cp2k.org                 #
-# SPDX-License-Identifier: GPL-2.0+                                           #
-###############################################################################
+####################################################################################################
+# Copyright (C) by the DBCSR developers group - All rights reserved                                #
+# This file is part of the DBCSR library.                                                          #
+#                                                                                                  #
+# For information on the license, see the LICENSE file.                                            #
+# For further information please visit https://dbcsr.cp2k.org                                      #
+# SPDX-License-Identifier: GPL-2.0+                                                                #
+####################################################################################################
 #
 # This script is based on OpenTuner's tutorial
 # "Optimizing Block Matrix Multiplication", and
