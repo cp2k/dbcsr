432ff631a376ea6324d31d1d9eccb143e55b5a42
Hans Pabst <hans.pabst@intel.com>
2021-01-22 08:53:53 +0100

    Further adjusted build/run scripts (Daint-CI).

--- .ci/daint.cscs.ch/ocl.build.sh
+++ .ci/daint.cscs.ch/ocl.build.sh
@@ -19,33 +19,36 @@ module unload cray-libsci_acc
 module list
 
 # Make LIBXSMM available
-git clone https://github.com/hfp/libxsmm.git
+if [ ! -d libxsmm ]; then
+  git clone https://github.com/hfp/libxsmm.git
+fi
 cd libxsmm
 git checkout 283207ab1cef052232e9a9c761bc6edfab9df290
 make -j
 export PKG_CONFIG_PATH=${HOME}/libxsmm/lib:${PKG_CONFIG_PATH}
 cd ..
 
-# help CMake to find the OpenCL implementation
-export NVSDKCOMPUTE_ROOT=${CUDATOOLKIT_HOME}
-
 set -o xtrace  # do not set earlier to avoid noise from module
 
 umask 0002  # make sure group members can access the data
 
-mkdir --mode=0775 -p "${SCRATCH}/${BUILD_TAG}.ocl"
+mkdir -p "${SCRATCH}/${BUILD_TAG}.ocl"
+chmod 0775 "${SCRATCH}/${BUILD_TAG}.ocl"
 cd "${SCRATCH}/${BUILD_TAG}.ocl"
+
+# help CMake to find the OpenCL implementation
+export NVSDKCOMPUTE_ROOT=${CUDATOOLKIT_HOME}
 
 cmake \
     -DCMAKE_SYSTEM_NAME=CrayLinuxEnvironment \
     -DCMAKE_CROSSCOMPILING_EMULATOR="" \
     -DUSE_ACCEL=opencl -DUSE_SMM=libxsmm \
-    -DOpenCL_LIBRARY=${CUDATOOLKIT_HOME}/lib64/libOpenCL.so \
+    -DOpenCL_LIBRARY="${CUDATOOLKIT_HOME}/lib64/libOpenCL.so" \
     -DBLAS_FOUND=ON -DBLAS_LIBRARIES="-lsci_gnu_mpi_mp" \
     -DLAPACK_FOUND=ON -DLAPACK_LIBRARIES="-lsci_gnu_mpi_mp" \
     -DMPIEXEC_EXECUTABLE="$(command -v srun)" \
-    -DTEST_MPI_RANKS=${SLURM_NTASKS} \
-    -DTEST_OMP_THREADS=${SLURM_CPUS_PER_TASK} \
+    -DTEST_MPI_RANKS="${SLURM_NTASKS}" \
+    -DTEST_OMP_THREADS="${SLURM_CPUS_PER_TASK}" \
     "${WORKSPACE}" |& tee -a "${STAGE_NAME}.out"
 
 make VERBOSE=1 -j |& tee -a "${STAGE_NAME}.out"
--- .ci/daint.cscs.ch/ocl.test.sh
+++ .ci/daint.cscs.ch/ocl.test.sh
@@ -22,7 +22,8 @@ set -o xtrace  # do not set earlier to avoid noise from module
 
 umask 0002  # make sure group members can access the data
 
-mkdir --mode=0775 -p "${SCRATCH}/${BUILD_TAG}.ocl"
+mkdir -p "${SCRATCH}/${BUILD_TAG}.ocl"
+chmod 0775 "${SCRATCH}/${BUILD_TAG}.ocl"
 cd "${SCRATCH}/${BUILD_TAG}.ocl"
 
 export CRAY_CUDA_MPS=1 # enable the CUDA proxy for MPI+CUDA
