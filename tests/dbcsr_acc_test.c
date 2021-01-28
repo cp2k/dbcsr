9f8cb2769e17353c303fe4df948e1bcf833a2bf6
Harmen Stoppels <harmenstoppels@gmail.com>
2021-01-28 11:37:51 +0100

    Merge hip / cuda cmake stuff

--- tests/dbcsr_acc_test.c
+++ tests/dbcsr_acc_test.c
@@ -6,7 +6,7 @@
  * For further information please visit https://dbcsr.cp2k.org                                    *
  * SPDX-License-Identifier: GPL-2.0+                                                              *
  *------------------------------------------------------------------------------------------------*/
-#include "../src/acc/acc.h"
+#include "acc/acc.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
