9792a8021f390834153a95ded4d9bdcbf998c611
Gina Sitaraman <gina.sitaraman@amd.com>
2021-05-25 02:26:05 -0500

    Add Mi100 support (#452)

--- src/acc/libsmm_acc/kernels/smm_acc_predict.py
+++ src/acc/libsmm_acc/kernels/smm_acc_predict.py
@@ -61,6 +61,7 @@ gpu_architectures = {
     "parameters_V100.json": "sm_70",
     "parameters_Vega10.json": "gfx900",
     "parameters_Mi50.json": "gfx906",
+    "parameters_Mi100.json": "gfx908",
 }
 
 
--- src/acc/libsmm_acc/tune/tune_setup.py
+++ src/acc/libsmm_acc/tune/tune_setup.py
@@ -406,9 +406,8 @@ def gen_makefile(outdir, compiler, arch):
                 + " -w -o $@ $^ -lcuda -lnvrtc\n\n"
             )
         else:
-            output += (
-                "\thipcc -O3 -D__HIP -w -o $@ $^ /opt/rocm/hip/lib/libhiprtc.so\n\n"
-            )
+            rocm_path = os.getenv("ROCM_PATH", "/opt/rocm")
+            output += f"\thipcc -O3 -D__HIP -w -o $@ $^ {rocm_path}/hip/lib/libamdhip64.so\n\n"
 
     # write Makefile
     writefile(outdir + "/Makefile", output)
