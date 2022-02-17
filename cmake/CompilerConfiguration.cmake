8fe7bbb669c8dea4a2d18a4fd95d173d0d75e279
Alfio Lazzaro <alfio.lazzaro@gmail.com>
2022-02-17 15:26:35 +0100

    Add -fallow-argument-mismatch for GCC 10+

--- cmake/CompilerConfiguration.cmake
+++ cmake/CompilerConfiguration.cmake
@@ -2,6 +2,8 @@ if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   set(CMAKE_Fortran_FLAGS          "-ffree-form -std=f2008ts -fimplicit-none -Werror=aliasing -Werror=ampersand -Werror=c-binding-type -Werror=intrinsic-shadow -Werror=intrinsics-std -Werror=line-truncation -Werror=tabs -Werror=target-lifetime -Werror=underflow -Werror=unused-but-set-parameter -Werror=unused-but-set-variable -Werror=unused-variable -Werror=unused-dummy-argument -Werror=conversion -Werror=zerotrip -Werror=uninitialized -Wno-maybe-uninitialized -Werror=unused-parameter")
   if (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 10)
     set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Werror=argument-mismatch")  # gcc 10+ has this automatically
+   else ()
+   set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} fallow-argument-mismatch") # requires for 10+ for the MPI wrap module
   endif ()
   set(CMAKE_Fortran_FLAGS_RELEASE  "-O3 -g -funroll-loops")
   set(CMAKE_Fortran_FLAGS_COVERAGE "-O0 -g --coverage -fno-omit-frame-pointer -fcheck=all -ffpe-trap=invalid,zero,overflow -fbacktrace -finit-real=snan -finit-integer=-42 -finit-derived -Werror=realloc-lhs -finline-matmul-limit=0 -Werror")
