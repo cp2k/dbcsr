aabd30436a3cc9d56aafc52f6f623f3c144b4305
Alfio Lazzaro <alfio.lazzaro@gmail.com>
2022-02-18 16:55:50 +0100

    Update CMake on Daint-ci to a newer version

--- .ci/daint.cscs.ch/ocl.build.sh
+++ .ci/daint.cscs.ch/ocl.build.sh
@@ -14,9 +14,10 @@ set -o pipefail
 
 module swap PrgEnv-cray PrgEnv-gnu
 module load daint-gpu cudatoolkit cdt-cuda
-module load /apps/daint/UES/jenkins/7.0.UP02/gpu/easybuild/modules/all/CMake/3.18.4
 module unload cray-libsci_acc
 module list
+
+export PATH=/project/cray/alazzaro/cmake/bin:$PATH
 
 # Checkout and build LIBXSMM
 if [ ! -d "${HOME}/libxsmm" ]; then
