67b3c192fc8d25548b87a22bb2368f5d176d2927
Hans Pabst <hans.pabst@intel.com>
2024-05-29 09:25:45 +0200

    ocl: always print number of devices found
    * Using the CUDA backend, ACC_OPENCL_VERBOSE is not available.
    * Prints the activated device and number of found devices.
    * Helps, e.g., CI to capture basic information.

--- src/acc/acc_bench_smm.c
+++ src/acc/acc_bench_smm.c
@@ -227,9 +227,7 @@ int main(int argc, char* argv[]) {
     int ndevices = 0;
     result = c_dbcsr_acc_get_ndevices(&ndevices);
     if (0 < ndevices && (0 == device || EXIT_SUCCESS == c_dbcsr_acc_set_active_device(device))) {
-#if defined(_DEBUG)
-      fprintf(stderr, "Activated device %i of %i (device%i).\n", device + 1, ndevices, device);
-#endif
+      PRINTF("Activated device%i (ndevices=%i)\n", device, ndevices);
     }
     else {
       if (0 >= ndevices) {
