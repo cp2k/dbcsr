22f3ba9022b8a6654ef5350c29fa7643a5e66071
Hans Pabst <hans.pabst@intel.com>
2024-05-30 10:00:34 +0200

    ocl: changed license to BSD-3-Clause
    * Harmonize with CP2K/offload interface/component.
    * Added license header (pre-commit check).
    * Added LICENSE.md (src/acc/opencl).

--- src/acc/opencl/common/opencl_common.h
+++ src/acc/opencl/common/opencl_common.h
@@ -4,7 +4,7 @@
 /*                                                                                                */
 /* For information on the license, see the LICENSE file.                                          */
 /* For further information please visit https://dbcsr.cp2k.org                                    */
-/* SPDX-License-Identifier: GPL-2.0+                                                              */
+/* SPDX-License-Identifier: BSD-3-Clause                                                          */
 /*------------------------------------------------------------------------------------------------*/
 #ifndef OPENCL_COMMON_H
 #define OPENCL_COMMON_H
