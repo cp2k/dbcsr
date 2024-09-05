b9465e3328f805b22264339deb6a4a22877d7c61
Sriranjani Sitaraman <Sriranjani.Sitaraman@amd.com>
2024-09-05 17:57:42 -0500

    Avoid Ctx API in HIP backend

--- src/acc/cuda_hip/acc_init.cpp
+++ src/acc/cuda_hip/acc_init.cpp
@@ -26,8 +26,10 @@ extern "C" int c_dbcsr_acc_init() {
   ACC_DRV(device) acc_device;
   ACC_API_CALL(GetDevice, (&myDevice));
   ACC_DRV_CALL(DeviceGet, (&acc_device, myDevice));
+#if defined(__CUDA)
   ACC_DRV(context) ctx;
   ACC_DRV_CALL(DevicePrimaryCtxRetain, (&ctx, acc_device));
+#endif
   ACC_API_CALL(RuntimeGetVersion, (&runtimeVersion));
 
   // Initialize libsmm_acc, DBCSR's GPU backend
@@ -41,6 +43,8 @@ extern "C" int c_dbcsr_acc_finalize() {
   ACC_DRV(device) acc_device;
   ACC_API_CALL(GetDevice, (&myDevice));
   ACC_DRV_CALL(DeviceGet, (&acc_device, myDevice));
+#if defined(__CUDA)
   ACC_DRV_CALL(DevicePrimaryCtxRelease, (acc_device));
+#endif
   return libsmm_acc_finalize();
 }
