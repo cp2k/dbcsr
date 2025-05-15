6017e605dca6fc0355c4dae365e05c285d721b8b
Hans Pabst <hans.pabst@intel.com>
2025-05-15 16:22:12 +0200

    ocl: runtime-enable printing in DBCSR profile
    - More detailed profile (c_dbcsr_acc_device_synchronize).
    - Denote function name prefix-offset (profile).
    - Enable ACC_OPENCL_PROFILE_DBCSR at compile-time.
    - Runtime-control ACC_OPENCL_PROFILE.
    - Removed ACC_OPENCL_PROFILE_TRANSFER.

--- src/acc/opencl/acc_opencl.h
+++ src/acc/opencl/acc_opencl.h
@@ -126,11 +126,11 @@
 #if !defined(ACC_OPENCL_ACTIVATE) && 0
 #  define ACC_OPENCL_ACTIVATE 0
 #endif
-/* Use DBCSR's profile for detailed timings */
-#if !defined(ACC_OPENCL_PROFILE_DBCSR) && (defined(__OFFLOAD_PROFILING) || 0)
-#  define ACC_OPENCL_PROFILE_DBCSR
-#elif !defined(ACC_OPENCL_PROFILE_TRANSFER) && 0
-#  define ACC_OPENCL_PROFILE_TRANSFER
+/* Use DBCSR's profile for detailed timings (function name prefix-offset) */
+#if !defined(ACC_OPENCL_PROFILE_DBCSR) && (defined(__OFFLOAD_PROFILING) || 1)
+#  if defined(__DBCSR_ACC)
+#    define ACC_OPENCL_PROFILE_DBCSR 8
+#  endif
 #endif
 
 #if defined(__OFFLOAD_OPENCL) && !defined(ACC_OPENCL_MEM_DEVPTR)
@@ -356,8 +356,14 @@ typedef struct c_dbcsr_acc_opencl_config_t {
   cl_int ndevices;
   /** Maximum number of threads (omp_get_max_threads). */
   cl_int nthreads;
-  /** How to apply/use stream priorities. */
+#if defined(ACC_OPENCL_STREAM_PRIORITIES)
+  /** Runtime-adjust ACC_OPENCL_STREAM_PRIORITIES. */
   cl_int priority;
+#endif
+#if defined(ACC_OPENCL_PROFILE_DBCSR)
+  /** Runtime-enable ACC_OPENCL_PROFILE_DBCSR. */
+  cl_int profile;
+#endif
   /** Configuration and execution-hints. */
   cl_int xhints;
   /** Asynchronous memory operations. */
