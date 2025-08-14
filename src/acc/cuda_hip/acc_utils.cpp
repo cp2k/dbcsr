305855243aaa6544e933136ef3072708f9d56d06
Franz Pöschel <franz.poeschel@gmail.com>
2025-08-14 14:27:19 +0200

    Same for HIP

--- src/acc/cuda_hip/acc_utils.cpp
+++ src/acc/cuda_hip/acc_utils.cpp
@@ -17,7 +17,9 @@
 
 #if defined(__parallel)
 #  include <mpi.h>
+#if defined(OPEN_MPI)
 #  include <mpi-ext.h>
+#endif
 #endif
 
 //===========================================================================
@@ -32,9 +34,26 @@ int acc_get_gpu_warp_size() {
 extern "C"
 {
   #if defined(__parallel)
-  int dbcsr_mpix_query_cuda_support()
-  {
-    return MPIX_Query_cuda_support();
-  }
-  #endif
+/*
+   * Need these bindings since some MPI implementations define these functions
+   * only for their C API.
+   *  Need to write wrappers for them since they may be defined as a Macro in C.
+   */
+int dbcsr_mpix_query_cuda_support() {
+#if defined(OPEN_MPI) || defined(MPICH)
+  return MPIX_Query_cuda_support();
+#else
+  return 0;
+#endif
+}
+int dbcsr_mpix_query_hip_support() {
+#if defined(OPEN_MPI)
+  return MPIX_Query_rocm_support();
+#elif defined(MPICH)
+  return MPIX_Query_hip_support();
+#else
+  return 0;
+#endif
+}
+#endif
 }
