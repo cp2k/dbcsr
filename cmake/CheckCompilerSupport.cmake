7d72f8c49d46dcbe263da7c2c7fbb1feb42037ba
Hans Pabst <hans.pabst@intel.com>
2026-05-19 12:46:31 +0200

    Better feature-check under macOS and suppress OpenMP warning
    - Set CMAKE_TRY_COMPILE_TARGET_TYPE to STATIC_LIBRARY (macOS), compiler test during configure need no linkage.
    - Save/restore CMAKE_Fortran_FLAGS around feature checks, to potentially run without -Werror flags.
    - Avoid error: ‘master’ construct is deprecated use ‘masked’ (since OpenMP 5.1).
    - Check -Wno-error=deprecated-openmp works even if GNU Fortran is 15.1+ (macOS).

--- cmake/CheckCompilerSupport.cmake
+++ cmake/CheckCompilerSupport.cmake
@@ -3,6 +3,9 @@ include(CheckFortranSourceCompiles)
 set(CHECK_PROGRAMS f2008-norm2.f90 f2008-block_construct.f90
                    f2008-contiguous.f90 f95-reshape-order-allocatable.f90)
 
+set(_saved_fortran_flags "${CMAKE_Fortran_FLAGS}")
+set(CMAKE_Fortran_FLAGS "")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 foreach (prog ${CHECK_PROGRAMS})
   get_filename_component(prog_ext ${prog} EXT) # get the src extension to pass
                                                # along
@@ -17,3 +20,5 @@ foreach (prog ${CHECK_PROGRAMS})
       FATAL_ERROR "Your compiler does not support all required F2008 features")
   endif ()
 endforeach ()
+unset(CMAKE_TRY_COMPILE_TARGET_TYPE)
+set(CMAKE_Fortran_FLAGS "${_saved_fortran_flags}")
