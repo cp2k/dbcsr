18ad79382c6f69b57580653de068ea0ce7e71016
Hans Pabst <hans.pabst@intel.com>
2026-05-19 13:57:15 +0200

    Reintegrate with LIBXS, LIBXSTREAM, and LIBXSMM
    - LIBXS (https://libxs.readthedocs.io/) foundational library.
      - Implements SMMs leveraging different JIT providers.
      - LIBXSMM to only provide JIT kernels.
      - MKL to also provide JIT-kernels.
    - LIBXSTREAM (https://libxstream.readthedocs.io/)
      - Revised OpenCL backend now formally separate.
      - Leverage LIBXS for kernel registry and tuning.
    - LIBXSMM: removed from Docker images
      - Auto-download and find/build like LIBXS/XSTREAM.

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,7 @@
+set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
+set(LIBXS_GIT_TAG "1d776c091fb41d4aa7bc92335f6387878e1a9db6")
+
+set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
+set(LIBXSTREAM_GIT_TAG "87bbd69f0cdf6ca23801923ee201f960fb37860f")
+
+set(LIBXSMM_GIT_TAG "0cea22fdc34ec54bc59ffb47a43cb3e28b26d3e0")
