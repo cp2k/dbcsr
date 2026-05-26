3560077c25817c50d67deb33bd065c5229044741
Hans Pabst <hans.pabst@intel.com>
2026-05-26 16:54:17 +0200

    Updated LIBXS/LIBXSTREAM
    - Define __BLAS when LIBXS is enabled.
    - Revised code format.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,7 @@
 set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
-set(LIBXS_GIT_TAG "1d776c091fb41d4aa7bc92335f6387878e1a9db6")
+set(LIBXS_GIT_TAG "691ec5b5e5b5d341e9c91009ee5647f7e32b3b5e")
 
 set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
-set(LIBXSTREAM_GIT_TAG "87bbd69f0cdf6ca23801923ee201f960fb37860f")
+set(LIBXSTREAM_GIT_TAG "4c00f024f8f712065605f830356dc56c246d6f54")
 
 set(LIBXSMM_GIT_TAG "0cea22fdc34ec54bc59ffb47a43cb3e28b26d3e0")
