1fa81a029cdc9cf59e578994015a8b90835c3dcc
Hans Pabst <hans.pabst@intel.com>
2026-06-03 15:24:44 +0200

    Tweaked CMake configuration
    - LIBXS, LIBXSTREAM, and LIBXSMM to follow BUILD_SHARED_LIBS.
    - Updated LIBXS and LIBXSTREAM.
    - Fixed static linkage.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,7 @@
 set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
-set(LIBXS_GIT_TAG "db4bbb98754f467b4c3f7134ef6f8f65ea63af2e")
+set(LIBXS_GIT_TAG "1a9fad72958a07f28e7514c19328162485c3c358")
 
 set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
-set(LIBXSTREAM_GIT_TAG "afb97132fffe84c2dc1c050d3e0fb98ba6fad869")
+set(LIBXSTREAM_GIT_TAG "eb9acbb55f83e65572add7569c2aa0a6f38a5a41")
 
 set(LIBXSMM_GIT_TAG "0cea22fdc34ec54bc59ffb47a43cb3e28b26d3e0")
