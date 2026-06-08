bdc7c25b5cef73c714d794ad482114dd9f852dc5
Growl <uwsy1059@qq.com>
2026-06-08 18:16:17 +0800

    Update dependency

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -6,11 +6,11 @@
 
 # set(LIBXSMM_GIT_TAG "0cea22fdc34ec54bc59ffb47a43cb3e28b26d3e0")
 
-set(LIBXS_GIT_REPOSITORY "https://github.com/Growl1234/libxs.git")
-set(LIBXS_GIT_TAG "f54b9cf0261b9c63be56e452526b64b861533d41")
+set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
+set(LIBXS_GIT_TAG "11348a69c6b8622f9192ec725b291f42384a5ce7")
 
-set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/Growl1234/libxstream.git")
-set(LIBXSTREAM_GIT_TAG "761001e70698870cb9b0e380283d1945bb11301d")
+set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
+set(LIBXSTREAM_GIT_TAG "8375c018d0619bfd9261f67b9ed8b609d8e4ff66")
 
 set(LIBXSMM_GIT_REPOSITORY "https://github.com/Growl1234/libxsmm.git")
-set(LIBXSMM_GIT_TAG "4c2afa3c7745d15caa792334a3ed5a1995f249e6")
+set(LIBXSMM_GIT_TAG "47548ddea6e5b63c3602b51100660c14c8b2d54c")
