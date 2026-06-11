1ba44db5eff2f9ac549fb84eba61acd9f24f2071
SY Wang <uwsy1059@qq.com>
2026-06-11 16:52:34 +0800

    CMake cleanups and RPM-build fixing (#1018)
    * CMake: fix LIBXS and LIBXSTREAM discovery and revise default option
    * Default MPI_F08 OFF
    * Adjust GitHub Actions workflow
    * Adjust Fedora RPM-build
    * Switch to FetchContent if no OpenCL helper found in libxstream
    * Move dependency finding into Find*.cmake
    * Test CMake-built libxs* library
    * Update dependency
    * Remove FindLIBXS*.cmake and adjust workflow
    * Update dependency in CI
    * Incorporated hfp's changes
    * CMake build system cleanup
    * Update dependencies in CI
    * Avoid sending empty states to acc
    * Update libxstream (executable tool_opencl.sh)
    * Define __LIBXS for accelerator tests
    * Coverage: Skip libxs* files combined into DBCSR build
    * Update dependencies
    * Decide which target for libxsmm to use after find_package
    * Deal with libxsmm config package target
    * Fix libxsmm taget
    * Follow-up clean-ups
    * Update dependencies
    * Accommodate LIBXSMM's improved CMake
    * Remove FindLIBXSMM.cmake
    * Let LIBXSMM fallback to PkgConfig
    * Revise LIBXS-JIT and warning message
    * Update LIBXS option handling for OpenCL support
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Hans Pabst <hans.pabst@intel.com>
    Co-authored-by: Mathieu Taillefumier <mathieu.taillefumier@free.fr>

--- cmake/dependencies.cmake
+++
@@ -1,7 +0,0 @@
-set(LIBXS_GIT_REPOSITORY "https://github.com/hfp/libxs.git")
-set(LIBXS_GIT_TAG "1a9fad72958a07f28e7514c19328162485c3c358")
-
-set(LIBXSTREAM_GIT_REPOSITORY "https://github.com/hfp/libxstream.git")
-set(LIBXSTREAM_GIT_TAG "eb9acbb55f83e65572add7569c2aa0a6f38a5a41")
-
-set(LIBXSMM_GIT_TAG "0cea22fdc34ec54bc59ffb47a43cb3e28b26d3e0")
